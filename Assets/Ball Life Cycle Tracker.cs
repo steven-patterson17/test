36e9e796fc52f93281aa1ecd88ff7b00e40a07b4
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:21:03 -0400

    Update Ball Life Cycle Tracker.cs
    Remove namespace VR training if necessary, require for fixing reliability issues on SonarQube

--- Assets/Ball Life Cycle Tracker.cs
+++ Assets/Ball Life Cycle Tracker.cs
@@ -5,59 +5,62 @@ using UnityEngine;
 /// Tracks the cycle of the ball to make sure the ball is
 /// hit or missed
 /// </summary>
-public class BallLifecycleTracker : MonoBehaviour
+namespace VRTraining
 {
-    /// <summary>
-    /// Indicates whether the ball has been successfully hit by the paddle.
-    /// </summary>
-    private bool hitPaddle = false;
-
-    /// <summary>
-    /// Subscribes to the ball return event when the object is enabled.
-    /// </summary>
-    void OnEnable()
+    public class BallLifecycleTracker : MonoBehaviour
     {
-        BallReturnMetricsProvider.OnBallReturn += OnBallReturn;
-    }
-
-    /// <summary>
-    /// Unsubscribes from the ball return event when the object is disabled
-    /// to prevent memory leaks or unintended callbacks.
-    /// </summary>
-    void OnDisable()
-    {
-        BallReturnMetricsProvider.OnBallReturn -= OnBallReturn;
-    }
-
-    /// <summary>
-    /// Event handler triggered when the ball is successfully returned by the paddle.
-    /// Marks the ball as having been hit.
-    /// </summary>
-    /// <param name="speed">The speed of the ball at the time of return.</param>
-    /// <param name="spin">The spin applied to the ball.</param>
-    /// <param name="angle">The return angle of the ball.</param>
-    /// <param name="distance">The distance traveled after the return.</param>
-    private void OnBallReturn(string swingType, float speed, float spin, float angle, float distance)
-    {
-        // This event only fires for THIS ball if the provider is on the ball
-        hitPaddle = true;
-    }
-
-    /// <summary>
-    /// Called when the ball object is destroyed.
-    /// If the ball was not hit by the paddle, registers a miss
-    /// with the session metrics manager.
-    /// </summary>
-    void OnDestroy()
-    {
-        if (!hitPaddle)
+        /// <summary>
+        /// Indicates whether the ball has been successfully hit by the paddle.
+        /// </summary>
+        private bool hitPaddle = false;
+    
+        /// <summary>
+        /// Subscribes to the ball return event when the object is enabled.
+        /// </summary>
+        void OnEnable()
         {
-            // This ball never hit the paddle → it's a miss
-            var session = FindFirstObjectByType<SessionMetricsManager>();
-
-            if (session != null)
+            BallReturnMetricsProvider.OnBallReturn += OnBallReturn;
+        }
+    
+        /// <summary>
+        /// Unsubscribes from the ball return event when the object is disabled
+        /// to prevent memory leaks or unintended callbacks.
+        /// </summary>
+        void OnDisable()
+        {
+            BallReturnMetricsProvider.OnBallReturn -= OnBallReturn;
+        }
+    
+        /// <summary>
+        /// Event handler triggered when the ball is successfully returned by the paddle.
+        /// Marks the ball as having been hit.
+        /// </summary>
+        /// <param name="speed">The speed of the ball at the time of return.</param>
+        /// <param name="spin">The spin applied to the ball.</param>
+        /// <param name="angle">The return angle of the ball.</param>
+        /// <param name="distance">The distance traveled after the return.</param>
+        private void OnBallReturn(string swingType, float speed, float spin, float angle, float distance)
+        {
+            // This event only fires for THIS ball if the provider is on the ball
+            hitPaddle = true;
+        }
+    
+        /// <summary>
+        /// Called when the ball object is destroyed.
+        /// If the ball was not hit by the paddle, registers a miss
+        /// with the session metrics manager.
+        /// </summary>
+        void OnDestroy()
+        {
+            if (!hitPaddle)
             {
-                session.RegisterMiss();
+                // This ball never hit the paddle → it's a miss
+                var session = FindFirstObjectByType<SessionMetricsManager>();
+    
+                if (session != null)
+                {
+                    session.RegisterMiss();
+                }
             }
         }
     }
