d134d0e96f61d547c76401142a01d0b8fcad83b6
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:56:17 -0400

    Update MetricBoard.cs
    Remove "namespace VRTraining" if necessary, require for fixing reliability issues on SonarQube

--- Assets/MetricBoard.cs
+++ Assets/MetricBoard.cs
@@ -1,53 +1,56 @@
 using UnityEngine;
 using TMPro;
 
-public class MetricsBoardUI : MonoBehaviour
+namespace VRTraining
 {
-    public static MetricsBoardUI Instance { get; private set; }
-
-    void Awake()
+    public class MetricsBoardUI : MonoBehaviour
     {
-        Instance = this;
-    }
-
-    public BallSpeedProvider speedProvider;
-    public BallDistanceProvider distanceProvider;
-
-    public TextMeshProUGUI speedText;
-    public TextMeshProUGUI distanceText;
-    public TextMeshProUGUI returnAngleText;
-    public TextMeshProUGUI returnSpeedText;
-    public TextMeshProUGUI returnSpinText;
-    public TextMeshProUGUI swingTypeText;
-
-    void Update()
-    {
-        if (speedProvider != null && speedText != null)
+        public static MetricsBoardUI Instance { get; private set; }
+    
+        void Awake()
         {
-            speedText.text = "Speed: " + speedProvider.Speed.ToString("F2") + " m/s";
+            Instance = this;
         }
-
-        if (distanceProvider != null && distanceText != null)
+    
+        public BallSpeedProvider speedProvider;
+        public BallDistanceProvider distanceProvider;
+    
+        public TextMeshProUGUI speedText;
+        public TextMeshProUGUI distanceText;
+        public TextMeshProUGUI returnAngleText;
+        public TextMeshProUGUI returnSpeedText;
+        public TextMeshProUGUI returnSpinText;
+        public TextMeshProUGUI swingTypeText;
+    
+        void Update()
         {
-            distanceText.text = "Distance: " + distanceProvider.Distance.ToString("F2") + " m";
+            if (speedProvider != null && speedText != null)
+            {
+                speedText.text = "Speed: " + speedProvider.Speed.ToString("F2") + " m/s";
+            }
+    
+            if (distanceProvider != null && distanceText != null)
+            {
+                distanceText.text = "Distance: " + distanceProvider.Distance.ToString("F2") + " m";
+            }
         }
-    }
-
-    public void SetReturnMetrics(float angle, float speed, float spin)
-    {
-        if (returnAngleText != null)
-            returnAngleText.text = "Return Angle: " + $"{angle:F2}°";
-
-        if (returnSpeedText != null)
-            returnSpeedText.text = "Return Speed: " + $"{speed:F2} m/s";
-
-        if (returnSpinText != null)
-            returnSpinText.text = "Return Spin: " + $"{spin:F2} rad/s";
-    }
-
-    public void SetSwingType(string swingtype)
-    {
-        if (swingTypeText != null)
-            swingTypeText.text = "Swing : " + swingtype;
+    
+        public void SetReturnMetrics(float angle, float speed, float spin)
+        {
+            if (returnAngleText != null)
+                returnAngleText.text = "Return Angle: " + $"{angle:F2}Â°";
+    
+            if (returnSpeedText != null)
+                returnSpeedText.text = "Return Speed: " + $"{speed:F2} m/s";
+    
+            if (returnSpinText != null)
+                returnSpinText.text = "Return Spin: " + $"{spin:F2} rad/s";
+        }
+    
+        public void SetSwingType(string swingtype)
+        {
+            if (swingTypeText != null)
+                swingTypeText.text = "Swing : " + swingtype;
+        }
     }
 }
