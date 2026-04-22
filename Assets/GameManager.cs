ef86a6716f1bf86862d104e5822bcad972ad5373
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:53:26 -0400

    Update GameManager.cs
    Remove "namespace VRTraining" if necessary, require for fixing reliability issues on SonarQube

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,34 +8,37 @@ using UnityEngine;
 /// Manages the panels that show up when users switches
 /// to a gamemode or ends the session
 /// </summary>
-public class MenuManager : MonoBehaviour
+namespace VRTraining
 {
-    /// <summary>
-    /// The main menu panel displayed at startup.
-    /// </summary>
-    [Header("Panels")]
-    public GameObject mainPanel;
-
-    /// <summary>
-    /// The panel that displays available game types.
-    /// </summary>
-    public GameObject gameTypesPanel;
-
-    /// <summary>
-    /// Opens the game types panel and hides the main menu panel.
-    /// </summary>
-    public void OpenGameTypes()
+    public class MenuManager : MonoBehaviour
     {
-        mainPanel.SetActive(false);
-        gameTypesPanel.SetActive(true);
-    }
-
-    /// <summary>
-    /// Closes the game types panel and returns to the main menu panel.
-    /// </summary>
-    public void CloseGameTypes()
-    {
-        gameTypesPanel.SetActive(false);
-        mainPanel.SetActive(true);
+        /// <summary>
+        /// The main menu panel displayed at startup.
+        /// </summary>
+        [Header("Panels")]
+        public GameObject mainPanel;
+    
+        /// <summary>
+        /// The panel that displays available game types.
+        /// </summary>
+        public GameObject gameTypesPanel;
+    
+        /// <summary>
+        /// Opens the game types panel and hides the main menu panel.
+        /// </summary>
+        public void OpenGameTypes()
+        {
+            mainPanel.SetActive(false);
+            gameTypesPanel.SetActive(true);
+        }
+    
+        /// <summary>
+        /// Closes the game types panel and returns to the main menu panel.
+        /// </summary>
+        public void CloseGameTypes()
+        {
+            gameTypesPanel.SetActive(false);
+            mainPanel.SetActive(true);
+        }
     }
 }
