3b58462c476da436e992190fccdd622f8b7da84c
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:56:43 -0400

    Update MovementScript.cs
    Remove "namespace VRTraining" if necessary, require for fixing reliability issues on SonarQube

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 /// Controls how the user is able to move around
 /// the unity scene
 /// </summary>
-public class PlayerMovement : MonoBehaviour
+namespace VRTraining
 {
-    public float speed = 3f;
-    public Transform cameraTransform; // assign CenterEyeAnchor
-
-    private Rigidbody rb;
-
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        // Read input (example: left thumbstick)
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        // Move relative to headset direction
-        Vector3 forward = cameraTransform.forward;
-        forward.y = 0;
-        forward.Normalize();
-
-        Vector3 right = cameraTransform.right;
-        right.y = 0;
-        right.Normalize();
-
-        Vector3 move = (forward * y + right * x) * speed;
-
-        rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
+        public float speed = 3f;
+        public Transform cameraTransform; // assign CenterEyeAnchor
+    
+        private Rigidbody rb;
+    
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+    
+        void FixedUpdate()
+        {
+            // Read input (example: left thumbstick)
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+    
+            // Move relative to headset direction
+            Vector3 forward = cameraTransform.forward;
+            forward.y = 0;
+            forward.Normalize();
+    
+            Vector3 right = cameraTransform.right;
+            right.y = 0;
+            right.Normalize();
+    
+            Vector3 move = (forward * y + right * x) * speed;
+    
+            rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
+        }
     }
 }
